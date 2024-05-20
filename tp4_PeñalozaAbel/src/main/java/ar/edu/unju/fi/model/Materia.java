ca4cdde3462864a5b5a626935b3225450c51a5ad
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-05-20 19:36:19 -0300

    Primer commit de la rama dev-1
    Autor: Abel Penhaloza

--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,5 +1,11 @@
 package ar.edu.unju.fi.model;
 
 public class Materia {
-
+	private int codigo;
+	private String nombre;
+	private String curso;
+	private byte cantidadDeHoras;
+	private Modalidad modalidad;
+	private Docente docente;
+	private Carrera carrera;
 }
