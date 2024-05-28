b4a39cc8ff52fb3a549704f049c11064ab77bba9
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-05-28 08:54:20 -0300

    Commit de la nueva rama de TP 4 parte dos
    Descripcion: Se trabajo en CollectionCarrera, CarreraController, index y
    en la clase Carrera
    
    Autor: Abel N Penhaloza

---
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/collections/CollectionCarrera.java
@@ -0,0 +1,93 @@
+package ar.edu.unju.fi.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.model.Carrera;
+
+@Component
+public class CollectionCarrera {
+
+	private static List<Carrera> carreras = new ArrayList<Carrera>();
+
+	/**
+	 * Metodo estatico que devuelve un ArrayList de Objetos de la clase Carrera.
+	 **/
+	public static List<Carrera> getCarreras() {
+
+		if (carreras.isEmpty()) {
+
+			carreras.add(new Carrera(1, "APU", (byte) 3, true));
+			carreras.add(new Carrera(2, "Ingenieria Quimica", (byte) 5, true));
+			carreras.add(new Carrera(3, "Licenciatura en Sistemas", (byte) 5, true));
+			carreras.add(new Carrera(4, "TUDIVJ", (byte) 3, true));
+			carreras.add(new Carrera(5, "Ingenieria Electronica", (byte) 5, false));
+
+		}
+		return carreras;
+	}
+
+	/**
+	 * Metodo estatico que agrega un Objetos Carrera al ArrayList de Carreras.
+	 * 
+	 * @param carrera a agregar
+	 **/
+	public static void agregarCarrera(Carrera carrera) {
+		carreras.add(carrera);
+	}
+
+	/**
+	 * Metodo estatico que elimina un Objetos carrera del ArrayList de carreras
+	 * @param codigoCarrera.
+	 **/
+	public static void eliminarCarrera(int codigo) {
+		Iterator<Carrera> iterator = carreras.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().getCodigo() == codigo) {
+				iterator.remove();
+			}
+		}
+	}
+
+	/**
+	 * Metodo estatico que modifica un Objetos carrera con los nuevos valores
+	 * enviados en
+	 * 
+	 * @param carrera objeto con los valores de atributos modificados.
+	 **/
+	public static void modificarCarrera(Carrera carrera) {
+		for (Carrera c : carreras) {
+
+			if (c.getCodigo() == carrera.getCodigo()) {
+				c.setNombre(carrera.getNombre());
+				c.setCantidadAnio(carrera.getCantidadAnio());
+				c.setEstado(carrera.isEstado());
+			} else {
+				System.out.println("No se encuentra el codigo de la Carrera");
+			}
+		}
+	}
+
+	/**
+	 * Metodo estatico que busca un Objetos carrera dentro del ArrayList, el
+	 * criterio es por
+	 * @param codigo el codigo a buscar en el ArryList carreras.
+	 * @param return
+	 **/
+	public static Carrera buscarCarrera(int codigo) {
+
+		Predicate<Carrera> filterCodigo = c -> c.getCodigo()== codigo;
+		Optional<Carrera> carrera = carreras.stream().filter(filterCodigo).findFirst();
+		if (carrera.isPresent()) {
+			return carrera.get();
+		} else {
+			return null;
+		}
+
+	}
+}
