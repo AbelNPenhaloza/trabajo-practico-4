fe522613b14908402edec8fd021229f910d13a67
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-06-08 12:01:12 -0300

    Commit para optimizar el codigo script.js
    Descripción: Se modifico el codigo para que desde un solo archivo js se
    pueda pasar la informacion para los diferentes html del proyecto.
    
    Autor: Abel N Peñaloza

--- tp4_PeñalozaAbel/src/main/resources/static/js/script.js
+++ tp4_PeñalozaAbel/src/main/resources/static/js/script.js
@@ -1,10 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function(){
-	let deleteModal = document.getElementById('deleteModal');
-	deleteModal.addEventListener('show.bs.modal', function (event){
-		let button = event.relatedTarget;
-		let codCarrera = button.getAttribute('data-codCarrera');
-		let form = deleteModal.querySelector('form');
-		form.setAttribute('action', '/carrera/eliminar/' + codCarrera);	
-	});
+    let deleteModal = document.getElementById('deleteModal');
+    deleteModal.addEventListener('show.bs.modal', function (event){
+        let button = event.relatedTarget;
+        
+        // Check the type of the modal to determine the correct attribute
+        if (button.hasAttribute('data-codCarrera')) {
+            let codCarrera = button.getAttribute('data-codCarrera');
+            let form = deleteModal.querySelector('form');
+            form.setAttribute('action', '/carrera/eliminar/' + codCarrera);
+        } else if (button.hasAttribute('data-codMateria')) {
+            let codMateria = button.getAttribute('data-codMateria');
+            let form = deleteModal.querySelector('form');
+            form.setAttribute('action', '/materia/eliminar/' + codMateria);
+        } else if (button.hasAttribute('data-codAlumno')) {
+            let codAlumno = button.getAttribute('data-codAlumno');
+            let form = deleteModal.querySelector('form');
+            form.setAttribute('action', '/alumno/eliminar/' + codAlumno);
+        } else if (button.hasAttribute('data-codDocente')) {
+            let codDocente = button.getAttribute('data-codDocente');
+            let form = deleteModal.querySelector('form');
+            form.setAttribute('action', '/docente/eliminar/' + codDocente);
+        }
+    });
 });
