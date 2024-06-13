7efdbc896d27e51b7c3286cb451160b0b6713fc5
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-06-12 23:06:14 -0300

    Commit TP 5 rama Materia
    Descripcion: Se realizaron los cambios en materias. MateriaMapper,
    MateriaDTO, IMateriaService y MateriaServiceImpl
    
    Autor Abel Peñaloza

--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -1,5 +1,28 @@
 package ar.edu.unju.fi.dto;
 
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.model.Carrera;
+import ar.edu.unju.fi.model.Curso;
+import ar.edu.unju.fi.model.Docente;
+import ar.edu.unju.fi.model.Modalidad;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MateriaDTO {
+	private Integer codigo;
+	private String nombre;
+	private Curso curso;
+	private Byte cantidadDeHoras;
+	private Modalidad modalidad;
+
+	private Docente docente;
+
+	private Carrera carrera;
 
 }
