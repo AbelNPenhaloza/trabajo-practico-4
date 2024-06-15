50afcd78ec156cf1ba14edf741fc753d4399979e
Mario <mario300401@gmail.com>
2024-06-14 23:50:12 -0300

    Se agrega el Mapper y el modelo DTO para Docente
    Autor: Mario Rios

--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/mapper/DocenteMapper.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/mapper/DocenteMapper.java
@@ -1,5 +1,24 @@
 package ar.edu.unju.fi.mapper;
 
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import ar.edu.unju.fi.dto.DocenteDTO;
+import ar.edu.unju.fi.model.Docente;
+
+@Mapper(componentModel = "spring")
 public interface DocenteMapper {
+	
+	DocenteDTO toDocenteDTO(Docente docente);
+	
+	@InheritInverseConfiguration
+	Docente toDocente(DocenteDTO docenteDTO);
+	
+	List<DocenteDTO> toDocenteDTOs(List<Docente> docentes);
+	
+	List<Docente> toDocentes(List<DocenteDTO> DocenteDTOs);
 
 }
