57b30fa84a3641a3aff6c5e30f8ea53d477b5870
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-06-26 16:51:58 -0300

    Commit se trabajo sobre las tareas asignadas.
    Descripcion: Se realizaron validaciones en Materia y Materia Dto. Se
    realizaron actualizaciones en sevcieImpl y se actualizo el
    materiaController.
    
    Autor Abel Penhaloza

--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -1,28 +1,88 @@
 package ar.edu.unju.fi.dto;
 
-import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
 
+import ar.edu.unju.fi.model.Alumno;
 import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Curso;
 import ar.edu.unju.fi.model.Docente;
 import ar.edu.unju.fi.model.Modalidad;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Component
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class MateriaDTO {
-	private Integer codigo;
+	private Long idMateria;
+
+	@NotBlank(message = "Debe ingresar el nombre de la Materia")
+	@Size(min = 3, max = 100, message = "El nombre de la materia debe tener entre 3 y 100 caracteres")
+	@Pattern(regexp = "[a-z A-Z]*", message = "Debe ingresar unicamente letras")
 	private String nombre;
+
+	@NotNull(message = "Debe seleccionar un Curso")
+	@Enumerated(EnumType.STRING)
 	private Curso curso;
-	private Byte cantidadDeHoras;
+
+	@NotNull(message = "Debe ingresar la cantidad de horas de la Materia")
+	@Pattern(regexp = "^(?:[1-9]?[0-9]|200)$", message = "La cantidad de horas debe estar entre 1 y 200")
+	@Column(name = "cantidad_de_horas")
+	private Integer cantidadDeHoras;
+
+	@NotNull(message = "Debe seleccionar una Modalidad")
+	@Enumerated(EnumType.STRING)
 	private Modalidad modalidad;
 
+	@NotNull(message = "Debe seleccionar un estado")
+	private Boolean estado;
+
+	@NotNull(message = "Debe seleccionar un Docente")
 	private Docente docente;
 
+	@NotNull(message = "Debe seleccionar una Carrera")
 	private Carrera carrera;
+
+	@NotNull(message = "Debe seleccionar uno o mas Alumnos")
+	private List<Alumno> alumnos = new ArrayList<Alumno>();
+
+	/**
+	 * @param nombre
+	 * @param curso
+	 * @param cantidadDeHoras
+	 * @param modalidad
+	 * @param estado
+	 * @param docente
+	 * @param carrera
+	 * @param alumnos
+	 */
+	public MateriaDTO(
+			@NotBlank(message = "Debe ingresar el nombre de la Materia") @Size(min = 3, max = 100, message = "El nombre de la materia debe tener entre 3 y 100 caracteres") @Pattern(regexp = "[a-z A-Z]*", message = "Debe ingresar unicamente letras") String nombre,
+			@NotNull(message = "Debe seleccionar un Curso") Curso curso,
+			@NotNull(message = "Debe ingresar la cantidad de horas de la Materia") @Pattern(regexp = "^(?:[1-9]?[0-9]|200)$", message = "La cantidad de horas debe estar entre 1 y 200") Integer cantidadDeHoras,
+			@NotNull(message = "Debe seleccionar una Modalidad") Modalidad modalidad,
+			@NotNull(message = "Debe seleccionar un estado") Boolean estado,
+			@NotNull(message = "Debe seleccionar un Docente") Docente docente,
+			@NotNull(message = "Debe seleccionar una Carrera") Carrera carrera,
+			@NotNull(message = "Debe seleccionar uno o mas Alumnos") List<Alumno> alumnos) {
+		this.nombre = nombre;
+		this.curso = curso;
+		this.cantidadDeHoras = cantidadDeHoras;
+		this.modalidad = modalidad;
+		this.estado = estado;
+		this.docente = docente;
+		this.carrera = carrera;
+		this.alumnos = alumnos;
+	}
 
 }
--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImpl.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImpl.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ar.edu.unju.fi.collections.CollectionMateria;
 import ar.edu.unju.fi.dto.MateriaDTO;
 import ar.edu.unju.fi.mapper.MateriaMapper;
+import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.repository.MateriaRepository;
 import ar.edu.unju.fi.service.IMateriaService;
 
 @Service
@@ -16,34 +17,50 @@ public class MateriaServiceImpl implements IMateriaService {
 	@Autowired
 	private MateriaMapper materiaMapper;
 
+	@Autowired
+	private MateriaRepository materiaRepository;
+
 	@Override
 	public List<MateriaDTO> findAll() {
 
-		List<MateriaDTO> materiasDTO = materiaMapper.toMateriaDTOs(CollectionMateria.getMaterias());
+		List<MateriaDTO> materiasDTO = materiaMapper.toMateriaDTOs(materiaRepository.findAll());
 		return materiasDTO;
 	}
 
 	@Override
-	public MateriaDTO findById(Integer codigo) {
-		MateriaDTO materiaDTO = materiaMapper.toMateriaDTO(CollectionMateria.buscarMateria(codigo));
-		return materiaDTO;
+	public MateriaDTO findById(Long idMateria) {
+		// Materia materia = materiaRepository.findById(idCodigo).orElse(null);
+		// return materiaMapper.toMateriaDTO(materia);
+		return materiaMapper.toMateriaDTO(materiaRepository.findById(idMateria).get());
 	}
 
 	@Override
-	public boolean save(MateriaDTO materiaDTO) {
-		boolean respuesta = CollectionMateria.agregarMateria(materiaMapper.toMateria(materiaDTO));
-		return respuesta;
+	public Materia save(MateriaDTO materiaDTO) {
+		// Materia materia = materiaMapper.toMateria(materiaDTO);
+		// materiaRepository.save(materia);
+		// return true;
+		Materia materia = materiaRepository.save(materiaMapper.toMateria(materiaDTO));
+		return materia;
 	}
 
 	@Override
-	public void deleteById(Integer codigo) {
-		CollectionMateria.eliminarMateria(codigo);
-
+	public void deleteById(Long idMateria) {
+		//materiaRepository.deleteById(idCodigo);
+		Materia materia = materiaRepository.findById(idMateria).get();
+		materia.setEstado(false);
+		materiaRepository.save(materia);
 	}
 
 	@Override
-	public void edit(MateriaDTO materiaDTO) throws Exception {
-		CollectionMateria.modificarMateria(materiaMapper.toMateria(materiaDTO));
+	public void editarMateria(MateriaDTO materiaDTO) throws Exception {
+		/*
+		 * Materia materia = materiaMapper.toMateria(materiaDTO); if
+		 * (materiaRepository.existsById(materia.getIdCodigo())) {
+		 * materiaRepository.save(materia); } else { throw new
+		 * Exception("La materia con codigo: " + materia.getIdCodigo() + " no existe");
+		 * }
+		 */
+		materiaRepository.save(materiaMapper.toMateria(materiaDTO));
 
 	}
 }
