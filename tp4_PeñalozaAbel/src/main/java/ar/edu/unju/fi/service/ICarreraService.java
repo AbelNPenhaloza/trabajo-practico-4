914df99f27580fa36e1b051e5406ecf765922b22
tarifamauro <Dastan869@gmail.com>
2024-07-02 19:54:27 -0300

    Correciones en CarreraService en CarreraServiceImpl y en CarreraRepository

--- tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/service/ICarreraService.java
+++ tp4_PeñalozaAbel/src/main/java/ar/edu/unju/fi/service/ICarreraService.java
@@ -3,15 +3,15 @@ package ar.edu.unju.fi.service;
 import java.util.List;
 
 import ar.edu.unju.fi.dto.CarreraDTO;
-import ar.edu.unju.fi.model.Carrera;
 
 public interface ICarreraService {
 	
+	List<CarreraDTO> findAllActive();
 	List<CarreraDTO> findAll();
 
 	CarreraDTO findById(Integer idCarrera);
 
-	Carrera save(CarreraDTO carreraDTO);
+	CarreraDTO save(CarreraDTO carreraDTO);
 
 	void deleteById(Integer idCarrera);
 
